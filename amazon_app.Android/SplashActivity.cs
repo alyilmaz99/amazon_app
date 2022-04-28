9a80e789f515038fd7e3ecb56051aa508aa29775
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-04-28 03:46:56 +0300

    SplashScreen

---
+++ amazon_app.Android/SplashActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amazon_app.Droid
+{
+    [Activity(Label = "SplashActivity" ,MainLauncher = true,Theme ="@style/MyTheme.Splash", NoHistory =true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            await SimulateStartUp();
+        }
+
+        async Task SimulateStartUp()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(4));
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
