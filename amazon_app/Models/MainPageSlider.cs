96b266a06c86e8b6c78c33bfea86673b1f432711
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-05-02 19:47:08 +0300

    Slidebar model

---
+++ amazon_app/Models/MainPageSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace amazon_app.Models
+{
+    public class MainPageSlider
+    {
+        ImageSource imageSource;
+        public ImageSource ImageSource
+        {
+            get => imageSource;
+            set => imageSource = value;
+        }
+    }
+}
---
+++ amazon_app/Services/MainPageSliderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using amazon_app.Models;
+
+namespace amazon_app.Services
+{
+    public class MainPageSliderService
+    {
+        private ObservableCollection<MainPageSlider> photo;
+
+        public MainPageSliderService()
+        {
+            photo = new ObservableCollection<MainPageSlider>
+            {
+                new MainPageSlider{ImageSource ="slider1.jpeg"},
+                new MainPageSlider{ImageSource ="slider2.jpeg"},
+                new MainPageSlider{ImageSource ="slider3.jpeg"},
+                new MainPageSlider{ImageSource ="slider4.jpeg"},
+                new MainPageSlider{ImageSource ="slider5.jpeg"},
+            };
+        }
+        public ObservableCollection<MainPageSlider> Photo 
+        {
+            get => photo;
+            set => photo = value;
+        }
+    }
+}
